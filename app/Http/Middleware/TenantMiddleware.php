3a50c2abdc2f5312048d5c6916a4b073008282a2
HashimKhan923 <khanhash1994@gmail.com>
2025-05-15 20:54:21 +0500

    dwdwdcefced

--- app/Http/Middleware/TenantMiddleware.php
+++ app/Http/Middleware/TenantMiddleware.php
@@ -27,7 +27,7 @@ class TenantMiddleware
             // Retrieve the tenant_id from the session
     $tenantId = session('tenant_id');
 
-    return $tenantId;
+    return response()->json(['id'=>$tenantId]);
 
     // Find the tenant in the database
     $Tenant = Tenant::where('tenant_id', $tenantId)->first();
