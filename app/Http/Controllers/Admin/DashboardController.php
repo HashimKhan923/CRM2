eebb4524ee37787301dd7e144282600bcff66262
HashimKhan923 <khanhash1994@gmail.com>
2025-05-21 18:57:46 +0500

    dwdwewe

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,20 +8,22 @@ use App\Models\User;
 use App\Models\Department;
 use App\Models\Shift;
 use App\Models\Time;
+use App\Models\Role;
 use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        $UserCount = User::count();
-        $DepartmentCount = Department::count();
-        $ShiftCount = Shift::count();
-        $TodayAttendenceCount = Time::whereDate('created_at', Carbon::today('Asia/Karachi'))->count();
+        // $UserCount = User::count();
+        $Departments = Department::all();
+        $Shifts = Shift::all();
+        $Roles = Role::all();
+        // $TodayAttendenceCount = Time::whereDate('created_at', Carbon::today('Asia/Karachi'))->count();
 
         
 
-        return response()->json(['UserCount'=>$UserCount,'DepartmentCount'=>$DepartmentCount,'ShiftCount'=>$ShiftCount,'TodayAttendenceCount'=>$TodayAttendenceCount]);
+        return response()->json(['Departments'=>$Departments,'Shifts'=>$Shifts,'Roles'=>$Roles]);
 
 
     }
