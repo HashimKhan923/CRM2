5b366f4290c0ed5edb3d88b81ea8e6e064c063c1
HashimKhan923 <khanhash1994@gmail.com>
2025-06-17 19:58:32 +0500

    wdw

--- app/Http/Controllers/SuperAdmin/ContactUsController.php
+++ app/Http/Controllers/SuperAdmin/ContactUsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\SuperAdmin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ContactUs;
+use Mail;
+
 
 class ContactUsController extends Controller
 {
@@ -17,6 +19,23 @@ class ContactUsController extends Controller
             'phone_number' => $request->phone_number,
             'message' => $request->message,
         ]);
+
+        Mail::send(
+            'mails.contactus',
+            [
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+                'phone_number' => $request->phone_number,
+                'message1' => $request->message,
+            ],
+            function ($message) use ($request) { 
+                $message->from($request->email,'LockMyTime');
+                $message->to('support@lockmytimes.com');
+                $message->replyTo($request->email);
+                $message->subject('Contact');
+            }
+        );
 
         return response()->json(['message'=>'Submited Successfully!']);
     }
