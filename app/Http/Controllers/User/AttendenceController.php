2067a5c19c977d6c889323a18083e18500a4336b
HashimKhan923 <khanhash1994@gmail.com>
2025-07-24 18:37:24 +0500

    dsdwdwdasasddsdsddwddwswcs

--- app/Http/Controllers/User/AttendenceController.php
+++ app/Http/Controllers/User/AttendenceController.php
@@ -24,7 +24,7 @@ class AttendenceController extends Controller
         $this->geolocationService = $geolocationService;
     }
 
-    public function index(Request $request, $id)
+    public function index(Request $request, user_)
     {
         $attendences = Time::with(['user', 'breaks']) // Load related breaks
         ->whereDate('created_at', Carbon::today())
@@ -179,7 +179,7 @@ class AttendenceController extends Controller
     
         $totalShiftMinutes = $shiftEnd->diffInMinutes($shiftStart);
     
-        $timeRecord = Time::where('user_id', $id)
+        $timeRecord = Time::where('user_id', $user_id)
                           ->whereDate('created_at', Carbon::today('Asia/Karachi'))
                           ->first();
     
