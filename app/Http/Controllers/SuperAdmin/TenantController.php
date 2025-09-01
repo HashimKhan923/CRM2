a2592a9ac19ecc432797d35a5ef068f0473ed53d
HashimKhan923 <khanhash1994@gmail.com>
2025-09-01 18:33:27 +0500

    edfewrrwdwdddcsdw

--- app/Http/Controllers/SuperAdmin/TenantController.php
+++ app/Http/Controllers/SuperAdmin/TenantController.php
@@ -68,7 +68,7 @@ class TenantController extends Controller
                 ],
             ]);
 
-                $user->stripe_customer_id = $session->customer;
+                $user->stripe_customer_id = $session->customer_creation;
                 $user->save();
 
 
