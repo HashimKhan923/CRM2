ad50216120b66ce6c7ff2945ba069eec300b4523
HashimKhan923 <khanhash1994@gmail.com>
2025-09-02 14:16:55 +0500

    dwdweswsasawswe

--- app/Models/AdminSubscription.php
+++ app/Models/AdminSubscription.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class AdminSubscription extends Model
 {
     use HasFactory;
+
+        protected $fillable = [
+        'tenant_id','stripe_id', 'stripe_price_id', 'status', 'current_period_end'
+    ];
 }
