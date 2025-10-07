ec8d40d1a87f3afec383d5bcb01c6302fab7d58c
HashimKhan923 <khanhash1994@gmail.com>
2025-10-07 22:16:28 +0500

    dwewewwddwdsceddwd

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -303,9 +303,11 @@ class UserController extends Controller
             'employment_type' => $request->employment_type,
         ]);
 
+        LeaveBalance::where('user_id', $user->id)->delete();
+
         if($request->leave_types)
         {
-            LeaveBalance::where('user_id', $user->id)->delete();
+            
 
             foreach ($request->leave_types as $leave) {
             LeaveBalance::updateOrCreate(
@@ -320,7 +322,7 @@ class UserController extends Controller
 
                 ]
             );
-        }
+            }
         }
 
 
