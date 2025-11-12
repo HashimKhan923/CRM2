ab3100be2a7679916cb5c457547b69bc9b70a812
HashimKhan923 <khanhash1994@gmail.com>
2025-11-12 11:43:30 +0500

    wdweqwqswssqsswqs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('attendance:mark-absent-daily')->everyFiveMinutes();
+        // $schedule->command('attendance:mark-absent-daily')->everyFiveMinutes();
         // $schedule->command('attendance:fill-missing')->everyFiveMinutes();
 
     }
